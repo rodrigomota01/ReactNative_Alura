9c9c6d1e0b720d48dd78f0c4f687a1f70ac28116
Rodrigo Mota Sousa Ferreira <Rodrigo.Mota@sicoob.com.br>
2021-09-27 16:57:43 -0300

    segregação de componentes da cesta

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
 
-import topo from '../../assets/topo.png';
-import logo from '../../assets/logo.png';
-import Texto from "../componentes/Texto";
+import logo from '../../../assets/logo.png';
+import Texto from "../../componentes/Texto";
+import Topo from "../../../src/telas/Cesta/componentes/Topo";
 
 
 const width = Dimensions.get('screen').width;
 export default function Cesta() {
     return <> 
-    <Image source={topo} style={estilos.topo} />
-    <Texto style={estilos.titulo}>Detalhe da Cesta</Texto>
+   
+    <Topo />
 
     <View style={estilos.cesta}>
         <Texto style={estilos.nome}>Cesta de Verduras</Texto>
@@ -29,20 +29,7 @@ export default function Cesta() {
 }
 
 const estilos = StyleSheet.create({
-    topo: {
-        width: "100%",
-        height: 578 / 768 * width,
-    },
-    titulo: {
-        width: "100%",
-        position: "absolute",
-        textAlign: "center",
-        fontSize: 16,
-        lineHeight: 26,
-        color: "white",
-        fontWeight: "bold",
-        padding: 16
-    },
+  
     cesta: {
         paddingVertical: 8,
         paddingHorizontal: 16,
